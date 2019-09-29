659fdad862393f36477afd16eb59a19c0cee94f8
skylar siggard <siggardskylar@gmail.com>
2019-09-29 15:20:19 -0600

    done

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const massive = require('massive')
 const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET} = process.env
 const authCtrl = require('./controllers/authController')
 const treasureCtrl = require('./controllers/treasureController')
+const midCtrl = require('./middleware/authMiddleware')
 
 const app = express()
 
@@ -19,7 +20,10 @@ app.post('/auth/register', authCtrl.register)
 app.post('/auth/login', authCtrl.login)
 app.get('/auth/logout', authCtrl.logout)
 // treasure
-app.get('api/treasure/dragon', treasureCtrl.dragonTreasure)
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
+app.get('/api/treasure/user', treasureCtrl.getUserTreasure, midCtrl.userOnly)
+app.post('/api/treasure/user', midCtrl.usersOnly, treasureCtrl.addUserTreasure)
+app.get('/api/treasure/all', midCtrl.usersOnly, midCtrl.adminsOnly, treasureCtrl.getAllTreasure)
 
 massive(CONNECTION_STRING).then(db => {
     app.set('db', db) 
